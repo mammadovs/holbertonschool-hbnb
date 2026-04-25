388f2c84ab154649750f9d88a98c802a0823743a
mammadovs <12747@holbertonstudents.com>
2026-04-25 13:54:27 +0000

    3. Amenity Endpoints

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -9,7 +9,11 @@ def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
+    # Register Namespaces
     from app.api.v1.users import api as users_ns
+    from app.api.v1.amenities import api as amenities_ns
+
     api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
 
     return app
